96175d023b49e8f100816944baa86d03e1641db4
617998 <sinchuri.617998@swl.k12.oh.us>
2024-11-07 08:31:01 -0500

    2iin

--- src/InputHelper.java
+++ src/InputHelper.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class InputHelper {
+    public static String getNonZeroLenString(Scanner scan, String prompt) {
+        String input;
+        boolean check = false;
+
+        System.out.println(prompt);
+        do {
+            input = scan.nextLine();
+            if (input.length() != 0) {
+                check = true;
+            } else {
+                System.out.println("You have to enter something, try again.");
+            }
+        } while (!check);
+
+        return input;
+    }
 }
--- src/TaskOneGetUsername.java
+++ src/TaskOneGetUsername.java
@@ -1,2 +1,10 @@
-public class Main {
+import java.util.Scanner;
+
+public class TaskOneGetUsername {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String username = InputHelper.getNonZeroLenString(scan, "Enter username:");
+        System.out.println("The username you entered is: " + username);
+    }
 }
